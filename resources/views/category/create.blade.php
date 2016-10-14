4f06564bb73f4c9b9995169cc4b67810ed8bb4ac
Phoenix <ugendu04@gmail.com>
2016-10-14 13:55:40 +0100

    modified category views

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,38 @@
+@extends('category.master')
+
+@section('service_active')
+    active
+@stop
+
+@section('content')
+	<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="header">
+                        <h4 class="title">Add new Category</h4>
+                    </div>
+                    <div class="content">
+                        {{Form::open(['route' => 'store_category', 'method' => 'POST'])}}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Description</label>
+                                        <input type="text" class="form-control" placeholder="Category name" name="category" value="{{old('category')}}">
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-info btn-fill pull-right">Add Category</button>
+                            <div class="clearfix"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                @include('layouts.sessions')
+                @include('layouts.errors')
+            </div>
+        </div>
+    </div>
+	<button type="submit" class="btn btn-primary">Submit</button>
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+	<title>JAY FM</title>
+
+    <link rel="icon" type="image/png" href="/creativetim/img/favicon.ico">
+    <link href="/creativetim/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/creativetim/css/animate.min.css" rel="stylesheet"/>
+    <link href="/creativetim/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+    <link href="/creativetim/css/demo.css" rel="stylesheet" />
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="/creativetim/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    @yield('stylesheets')
+
+    <script src="/creativetim/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="/creativetim/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="/creativetim/js/bootstrap-checkbox-radio-switch.js"></script>
+    <script src="/creativetim/js/bootstrap-notify.js"></script>
+	<script src="/creativetim/js/light-bootstrap-dashboard.js"></script>
+	<script src="/creativetim/js/demo.js"></script>
+	<script src="/js/anims.js"></script>
+    @yield('scripts')
+
+</head>
+<body>
+    <div class="wrapper">
+        <!-- @include('layouts.sidebar') -->
+
+        <div class="main-panel">
+            <!-- @yield('navbar') -->
+
+            <div class="content">
+                @yield('content')
+            </div>
+
+            <!-- @include('layouts.footer') -->
+        </div>
+    </div>
+</body>
+</html>
