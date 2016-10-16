1c70ef57a173cfaa95d72fad1dc57a00f2212fe8
nanichang <nanipaul68@gmail.com>
2016-10-16 12:03:33 +0100

    change tariff prefix name case

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('tariff.master');
 });
 
-Route::group(['prefix' => 'Tariff'], function () {
+Route::group(['prefix' => 'tariff'], function () {
 
     Route::get('/', 'TariffController@create')->name('get_tariff');
     Route::post('/', 'TariffController@store')->name('store_tariff');
