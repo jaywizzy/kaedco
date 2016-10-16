7b26ef1dbf220097b84e8b8568fc7133144ab2e2
nanichang <nanipaul68@gmail.com>
2016-10-16 15:43:23 +0100

    added injection substation relationshp

--- app/AreaOffice.php
+++ app/AreaOffice.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class AreaOffice extends Model
 {
-    //
+	
+    // public function injectionSubstations() {
+    // 	return $this->hasMany('App\InjectionSubstation');
+    // }
 }
