6aa58dc734d64456f6fec3bd450d5fbf04caa938
dihweng <albertdihweng@yahoo.com>
2016-10-19 14:28:16 +0100

    modified substation view

--- resources/views/substation/create.blade.php
+++ resources/views/substation/create.blade.php
@@ -73,36 +73,6 @@
                 @include('layouts.sessions')
                 @include('layouts.errors')
             </div>
-
-            <div class="col-md-12">
-                <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th>Sub-Station Name</th>
-                            <th>Injection Nerc Code</th>
-                            <th>Injection Kaedc Code</th>
-                            <th>Area Office Nerc</th>
-                            <th>Area Office Kaedc</th>
-                            <th>Edit</th>
-                        </tr>
-                    </thead>
-                  
-                    <tbody>
-                        @foreach( $substations as $substation )
-                            <tr>                                    
-                                <td>{{ $substation->substation_name }} </td>
-                                <td>{{ $substation->injection_nerc_code }}</td>
-                                <td>{{ $substation->injection_kaedc_code }}</td>
-                                <td>{{ $substation->area_office_nerc }}</td>
-                                <td>{{ $substation->area_office_kaedc }}</td>
-                                <td>
-                                    <a class="btn btn-primary" href="{{ '/editsubstation/'.$substation->injection_nerc_code }}">Edit</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
         </div>
     </div>
 @stop
